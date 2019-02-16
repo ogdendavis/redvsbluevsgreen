0c447ad918104d856864449401c36965777c8484
Lucas Ogden-Davis <lucas@ogdendavis.com>
2019-02-16 18:05:35 -0500

    Add endpoints for subgroups of athletes

--- wp-content/themes/FoundationPress/front-page.php
+++ wp-content/themes/FoundationPress/front-page.php
@@ -12,8 +12,20 @@
 
       <div class="standings">
         <?php // This will hold the code for grabbing & displaying results from games.crossfit.com! Trying to use the REST api, now...
+        // I'll probably end up writing the front-end in javascript. Here are the URLs to use for GET requests:
+        // base: /tcf-athletes/v1 (may need to add /wp-json to beginning)
+        // everybody: /all (or any error in final bit of path will give all)
+        // by gender: /men, /women
+        // by team: /green, /blue/, /red
+        // All should return sorted by overall placement
         ?>
-
+        <?php
+          // Modify request by adding parameters to the end
+          // e.g. /tcf-athletes/v1/standings?teams=all&gender=male
+          $request = new WP_REST_Request( 'GET', '/tcf-athletes/v1/' );
+          $response = rest_do_request($request);
+          var_dump($response);
+        ?>
       </div>
 
       <div class="analysis">
--- wp-content/themes/FoundationPress/functions.php
+++ wp-content/themes/FoundationPress/functions.php
@@ -68,45 +68,95 @@ if ( function_exists('register_sidebar') )
   )
 );
 
-// Function to get leaderboard data and append team info
+// Function to get leaderboard data from games site and append team info
 function get_athletes() {
-    /*
-     * This whole thing uses 2018 data (for now). Update it once the Open
-     * officially starts!
-    */
+  /*
+   * This whole thing uses 2018 data (for now). Update it once the Open
+   * officially starts!
+  */
 
-    // Hard-code IDs of athletes by team
-    $red_team_ids = array("413402", "131824", "1297646", "931856", "1304621", "1167840", "404218", "521316", "1266833", "530374", "738293", "182909", "145716", "96588", "959720", "1312258", "1012382");
-    $blue_team_ids = array("1294483", "110387", "1269102", "108900", "796546", "1333866", "324345", "170108", "781668", "50884", "262288", "908366", "47227", "731205", "1207039", "1210751", "1297587");
-    $green_team_ids = array("665310", "665310", "498403", "300060", "1036743", "1185747", "1299276", "66647", "354357", "662788", "559773", "1092875", "944486", "517107", "913881", "327442", "932950");
+  // Hard-code IDs of athletes by team
+  $red_team_ids = array("413402", "131824", "1297646", "931856", "1304621", "1167840", "404218", "521316", "1266833", "530374", "738293", "182909", "145716", "96588", "959720", "1312258", "1012382");
+  $blue_team_ids = array("1294483", "110387", "1269102", "108900", "796546", "1333866", "324345", "170108", "781668", "50884", "262288", "908366", "47227", "731205", "1207039", "1210751", "1297587");
+  $green_team_ids = array("665310", "665310", "498403", "300060", "1036743", "1185747", "1299276", "66647", "354357", "662788", "559773", "1092875", "944486", "517107", "913881", "327442", "932950");
 
-    // Fetch live men's & women's leaderboards from games.crossfit.com
-    $all_men = json_decode(file_get_contents('https://games.crossfit.com/competitions/api/v1/competitions/open/2018/leaderboards?division=1&region=0&scaled=0&sort=0&occupation=0&page=1&affiliate=4259'))->leaderboardRows;
-    $all_women = json_decode(file_get_contents('https://games.crossfit.com/competitions/api/v1/competitions/open/2018/leaderboards?division=2&region=0&scaled=0&sort=0&occupation=0&page=1&affiliate=4259'))->leaderboardRows;
+  // Fetch live men's & women's leaderboards from games.crossfit.com
+  $all_men = json_decode(file_get_contents('https://games.crossfit.com/competitions/api/v1/competitions/open/2018/leaderboards?division=1&region=0&scaled=0&sort=0&occupation=0&page=1&affiliate=4259'))->leaderboardRows;
+  $all_women = json_decode(file_get_contents('https://games.crossfit.com/competitions/api/v1/competitions/open/2018/leaderboards?division=2&region=0&scaled=0&sort=0&occupation=0&page=1&affiliate=4259'))->leaderboardRows;
 
-    // Put all athletes into one big array!
-    $all_athletes = array_merge($all_men, $all_women);
+  // Put all athletes into one big array!
+  $all_athletes = array_merge($all_men, $all_women);
 
-    // Add team indicator to each entrant
-    foreach($all_athletes as $athlete) {
-      if( in_array($athlete->entrant->competitorId, $red_team_ids) ) {
-        $athlete->entrant->team = 'red';
-      }
-      elseif( in_array($athlete->entrant->competitorId, $blue_team_ids) ) {
-        $athlete->entrant->team = 'blue';
-      }
-      elseif( in_array($athlete->entrant->competitorId, $green_team_ids) ) {
-        $athlete->entrant->team = 'green';
-      }
+  // Add team indicator to each entrant
+  foreach($all_athletes as $athlete) {
+    if( in_array($athlete->entrant->competitorId, $red_team_ids) ) {
+      $athlete->entrant->team = 'red';
     }
+    elseif( in_array($athlete->entrant->competitorId, $blue_team_ids) ) {
+      $athlete->entrant->team = 'blue';
+    }
+    elseif( in_array($athlete->entrant->competitorId, $green_team_ids) ) {
+      $athlete->entrant->team = 'green';
+    }
+  }
 
-    // For REST API testing, just return the whole thing, unsorted, for now
-    return $all_athletes;
+  // Return all athletes, with teams appended
+  return $all_athletes;
 }
+
+
+// Function to sort athletes and return it per the request's parameters
+function sort_athletes( $request ) {
+  // Get athletes!
+  $athletes = get_athletes();
+
+  // Figure out how we'll sort them
+  switch ($request['sort']) {
+    case 'red':
+      $filter_field = 'team';
+      $filter_value = 'red';
+      break;
+    case 'blue':
+      $filter_field = 'team';
+      $filter_value = 'blue';
+      break;
+    case 'green':
+      $filter_field = 'team';
+      $filter_value = 'green';
+      break;
+    case 'men':
+      $filter_field = 'gender';
+      $filter_value = 'M';
+      break;
+    case 'women':
+      $filter_field = 'gender';
+      $filter_value = 'F';
+      break;
+    default:
+      // If filters don't match, by default return all athletes
+      return $athletes;
+  }
+
+  // Sort and return
+  return array_filter($athletes, function($athlete) use ($filter_field, $filter_value) {
+    return ($athlete->entrant->{$filter_field} == $filter_value);
+  });
+}
+
+
 // Register athlete info retrieval with REST API!
+// Generic route for all athletes, sorted by overall standing
 add_action( 'rest_api_init', function () {
-  register_rest_route( 'athletes', '/json', array(
+  register_rest_route( 'tcf-athletes/v1', '/all', array(
     'methods' => 'GET',
     'callback' => 'get_athletes',
+  ) );
+} );
+
+// Routes to sort by team
+add_action( 'rest_api_init', function () {
+  register_rest_route( 'tcf-athletes/v1', '/(?P<sort>\D+)', array(
+    'methods' => 'GET',
+    'callback' => 'sort_athletes',
   ) );
 } );
