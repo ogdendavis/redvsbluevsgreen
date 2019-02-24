258391766437edd401c568591b910835b8c78428
Lucas Ogden-Davis <lucas@ogdendavis.com>
2019-02-24 17:55:57 -0500

    Update front page styling

--- wp-content/themes/FoundationPress/front-page.php
+++ wp-content/themes/FoundationPress/front-page.php
@@ -27,92 +27,47 @@
   <div class="grid-container">
     <div class="grid-x grid-margin-x">
 
-      <div class="cell medium-8 small-12 leaderboard-container">
-        <?php // Top-level tabs ?>
-        <ul class="tabs" data-tabs id="leaderboard-tabs">
-          <li class="tabs-title is-active">
-            <a href="#teamScores" aria-selected="true">Teams</a>
+      <div class="cell medium-8 small-12 teamLeaderboard__container">
+
+        <?php // Get the team leaderboard, then display it as an accordion! ?>
+        <?php
+          $request = new WP_REST_Request( 'GET', '/tcf-athletes/v1/get-team-leaderboard' );
+  			  $response = rest_do_request($request);
+          // Pass the response to the browser, so I can play with it in JavaScript!
+          echo '<script>';
+          echo 'var teamLeaderboardData = ' . $response->data . ';';
+          echo '</script>';
+        ?>
+
+        <ul class="accordion" data-accordion data-allow-all-closed="true" id="teamAccordion" class="teamLeaderboard">
+          <!-- leaderboard.js will populate each item here  -->
+          <li class="accordion-item" id="teamAccordionFirst" data-accordion-item>
+            <a href="#" class="accordion-title teamLeaderboard__title">
+            </a>
+            <div class="accordion-content" data-tab-content>
+            </div>
           </li>
-          <li class="tabs-title">
-            <a href="#individualScores" aria-selected="false">Individuals</a>
+          <li class="accordion-item" id="teamAccordionSecond" data-accordion-item>
+            <a href="#" class="accordion-title teamLeaderboard__title">
+            </a>
+            <div class="accordion-content" data-tab-content>
+            </div>
+          </li>
+          <li class="accordion-item" id="teamAccordionThird" data-accordion-item>
+            <a href="#" class="accordion-title teamLeaderboard__title">
+            </a>
+            <div class="accordion-content" data-tab-content>
+            </div>
           </li>
         </ul>
 
-        <?php // Tab content, including sub-tabs ?>
-        <div class="tabs-content" data-tabs-content="leaderboard-tabs">
-
-          <div class="tabs-panel is-active" id="teamLeaderboard">
-            <?php // Get the team leaderboard, then display it as an accordion! ?>
-            <?php
-              $request = new WP_REST_Request( 'GET', '/tcf-athletes/v1/get-team-leaderboard' );
-      			  $response = rest_do_request($request);
-              // Pass the response to the browser, so I can play with it in JavaScript!
-              echo '<script>';
-              echo 'var teamLeaderboardData = ' . $response->data . ';';
-              echo '</script>';
-            ?>
-
-            <ul class="accordion" data-accordion data-allow-all-closed="true" id="teamAccordion">
-              <!-- leaderboard.js will populate each item here  -->
-              <li class="accordion-item" id="teamAccordionFirst" data-accordion-item>
-                <a href="#" class="accordion-title teamLeaderboard__title">
-                </a>
-                <div class="accordion-content" data-tab-content>
-                </div>
-              </li>
-              <li class="accordion-item" id="teamAccordionSecond" data-accordion-item>
-                <a href="#" class="accordion-title teamLeaderboard__title">
-                </a>
-                <div class="accordion-content" data-tab-content>
-                </div>
-              </li>
-              <li class="accordion-item" id="teamAccordionThird" data-accordion-item>
-                <a href="#" class="accordion-title teamLeaderboard__title">
-                </a>
-                <div class="accordion-content" data-tab-content>
-                </div>
-              </li>
-            </ul>
-
-          </div>
-
-          <div class="tabs-panel no-padding" id="individualScores">
-
-            <?php // The sub-tablist should be identical for teams and individuals, except for ids ?>
-
-            <div class="cell medium-4">
-              <ul class="tabs" data-tabs id="individual-leaderboard">
-                <li class="tabs-title is-active">
-                  <a href="#individual-overall-leaderboard" aria-selected="true">Overall</a>
-                </li>
-                <li class="tabs-title">
-                  <a href="#individual-women-leaderboard" aria-selected="false">Women</a>
-                </li>
-                <li class="tabs-title">
-                  <a href="#individual-men-leaderboard" aria-selected="false">Men</a>
-                </li>
-              </ul>
-            </div>
-
-            <div class="cell medium-8">
-              <div class="tabs-content" data-tabs-content="individual-leaderboard">
-                <div class="tabs-panel is-active" id="individual-overall-leaderboard">
-                  <p>This is the mixed-gender leaderboard!</p>
-                </div>
-                <div class="tabs-panel" id="individual-women-leaderboard">
-                  <p>This is the women's leaderboard!</p>
-                </div>
-                <div class="tabs-panel" id="individual-men-leaderboard">
-                  <p>This is the men's leaderboard!</p>
-                </div>
-              </div>
-            </div>
-
-          </div> <!-- tabs-panel -->
-        </div><!-- tabs-content -->
-      </div><!-- leaderboard-container -->
+        <div class="teamLeaderboard__footer">
+          Want to see how you stack up against athletes around the world? Go to <a href="https://games.crossfit.com/leaderboard/open/2019" target="_blank">games.crossfit.com</a>.
+        </div>
+      </div>
 
       <?php
+        // Get the most recent post, and display it
         $args = array('numberposts' => 1);
         $post = get_posts($args)[0];
         echo '<div class="cell medium-4 small-12 analysis__container" onclick="window.location=\'' . $post->guid . '\'">';
